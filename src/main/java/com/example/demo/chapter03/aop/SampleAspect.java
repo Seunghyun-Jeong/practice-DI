8651f6d8062f41e2243e0412b948494cddf55fae
Seunghyun-Jeong <dkskakdgka@gmail.com>
2024-06-13 21:01:41 +0900

    feat: Around Advice 추가

--- src/main/java/com/example/demo/chapter03/aop/SampleAspect.java
+++ src/main/java/com/example/demo/chapter03/aop/SampleAspect.java
@@ -2,7 +2,9 @@ package com.example.demo.chapter03.aop;
 
 import java.text.SimpleDateFormat;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -28,5 +30,17 @@ public class SampleAspect {
         System.out.println(new SimpleDateFormat("yyyy/MM/dd").format(new java.util.Date()));
         // 메서드 이름 출력
         System.out.println(String.format("메서드명:%s", joinPoint.getSignature().getName()));
+    }
+
+    @Around("execution(* com.example.demo.chapter03.used.*Greet.*(..))")
+    public Object aroundAdvice(ProceedingJoinPoint joinPoint)  throws Throwable {
+        // 시작 부분 표시
+        System.out.println("===== Around Advice =====");
+        System.out.println("▼▼▼ 처리전 ▼▼▼");
+        // 지정한 클래스의 메서드 실행
+        Object result = joinPoint.proceed();
+        System.out.println("▲▲▲ 처리후 ▲▲▲");
+        // 반환값을 돌려줄 필요가 있는 경우에는 Object 타입의 반환값을 돌려줍니다.
+        return result;
     }
 }
